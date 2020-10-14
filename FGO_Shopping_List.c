396a11e347cd495c9ce40f6f491694d00fd7b76a
William Schuy <william.schuy@pcc.edu>
2020-10-14 05:06:21 -0700

    Add files via upload

--- FGO_Shopping_List.c
+++ FGO_Shopping_List.c
@@ -12,6 +12,9 @@ Program usage:
 Greets user: Welcomes user and prompts them on what they want to do.
 Options: View Servant List, Edit Servant List, View Materials, Edit Materials, View and Edit QP, View and Edit Embers, Save list to file, Exit program.
 Note: Will need a reference file to read to load older list.
+
+Execution example: Ask for Servant (name or id), ask for CURRENT state (ascension -> Level -> skill 1 -> Skill 2 -> skill 3), ask for FINAL state (as before).
+open Servant CSV, scan each line (columns 2-5 for ascensions, 6-14 for skills) and add materials to final list. 
 */
 
 
@@ -38,7 +41,83 @@ void greetings(){
 	
 }
 
-
+const char * callItemName(int itemNumber){
+	static char* itemNames[] = 
+		{
+			"Proof of Hero",
+			"Evil Bone",
+			"Dragon Fang",
+			"Void\'s Dust",
+			"Fool\'s Chain",
+			"Deadly Poisonous Needle",
+			"Mystic Spinal Fluid",
+			"Stake of Wailing Night",
+			"Mystic Gunpowder",
+			"Seed of Yggdrasil",
+			"Ghost Lantern",
+			"Octuplet Crystals",
+			"Serpent Jewel",
+			"Phoenix Feather",
+			"Eternal Gear",
+			"Forbidden Page",
+			"Homunculus Baby",
+			"Meteor Horseshoe",
+			"Great Knight Medal",
+			"Shell of Reminiscence",
+			"Refined Magatama",
+			"Claw of Chaos",
+			"Heart of the Foreign God",
+			"Dragon\'s Reverse Scale",
+			"Spirit Root",
+			"Warhorse\'s Young Horn",
+			"Tearstone of Blood",
+			"Black Beast Grease",
+			"Lamp of Evil-Sealing",
+			"Scarab of Wisdom",
+			"Primordial Lanugo",
+			"Cursed Beast Gallstone",
+			"Mysterious Divine Wine",
+			"Saber Piece",
+			"Saber Monument",
+			"Archer Piece",
+			"Archer Monument",
+			"Lancer Piece",
+			"Lancer Monument",
+			"Rider Piece",
+			"Rider Monument",
+			"Caster Piece",
+			"Caster Monument",
+			"Assassin Piece ",
+			"Assassin Monument",
+			"Berserker Piece",
+			"Berserker Monument",
+			"Blue Gem Saber",
+			"Blue Gem Archer",
+			"Blue Gem Lancer",
+			"Blue Gem Rider",
+			"Blue Gem Caster",
+			"Blue Gem Assassin",
+			"Blue Gem Berserker",
+			"Red Gem Saber",
+			"Red Gem Archer",
+			"Red Gem Lancer",
+			"Red Gem Rider",
+			"Red Gem Caster",
+			"Red Gem Assassin",
+			"Red Gem Berserker",
+			"Yellow Gem Saber",
+			"Yellow Gem Archer",
+			"Yellow Gem Lancer",
+			"Yellow Gem Rider",
+			"Yellow Gem Caster",
+			"Yellow Gem Assassin",
+			"Yellow Gem Berserker",
+			"QP",
+			"Event Material"
+			};
+	int itemTotal[70] = {};
+	return itemNames[itemNumber];
+}
 
 void main(){
 	printf("Raz's Fate Grand Order Shopping List:\n");
